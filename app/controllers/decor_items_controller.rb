0084f80578dc9fb233545c5ceee4f19195d6c637
nasira <nasira@cybernest.com>
2026-02-18 13:02:59 +0500

    implemented user login and logout functionality

--- app/controllers/decor_items_controller.rb
+++ app/controllers/decor_items_controller.rb
@@ -2,5 +2,9 @@ class DecorItemsController < ApplicationController
   def index
     @decor_items = DecorItem.all.order(created_at: :desc)
   end
+
+  def show
+    @decor_item = DecorItem.find(params[:id])
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :decor_items, only: [:index]
+  resources :decor_items, only: [:index, :show]
 
   # Defines the root path route ("/")
   root "decor_items#index"
