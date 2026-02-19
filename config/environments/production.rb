843a7747a5ac048587d07429018754c233482dfe
nasira <nasira@cybernest.com>
2026-02-19 14:54:04 +0500

    precompile assets on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,16 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+
+  config.assets.precompile += %w(
+    controllers/application.js
+    controllers/hello_controller.js
+    controllers/reviews_subscription_controller.js
+    controllers/index.js
+    channels/consumer.js
+    channels/reviews_channel.js
+  )
 
   config.serve_static_assets = true
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
