769bd9ec08fbfda99ddf6c45417671cabd06c2ec
nasira <nasira@cybernest.com>
2026-02-19 15:31:49 +0500

    added logging

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 // app/javascript/application.js
+console.log("Application.js loaded");
 import "@hotwired/turbo-rails"
 import "@rails/actioncable"
 import "controllers"
