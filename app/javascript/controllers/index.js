4b836ff1ffc093796cb6b75794f01cd52869fdcc
nasira <nasira@cybernest.com>
2026-02-20 12:47:08 +0500

    fix

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,5 @@
 // app/javascript/controllers/index.js
-import { application } from "./application"
-import ReviewsSubscriptionController from "./reviews_subscription_controller"
+import { application } from "controllers/application"  // ✅ goes through importmap
+import ReviewsSubscriptionController from "controllers/reviews_subscription_controller"
 
 application.register("reviews-subscription", ReviewsSubscriptionController)
