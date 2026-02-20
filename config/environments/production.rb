4079e82b80afe931de62c8275d534b03d2b23476
nasira <nasira@cybernest.com>
2026-02-20 10:59:43 +0500

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,6 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.digest = true
   config.assets.version = '1.0'
 
   config.serve_static_assets = true
