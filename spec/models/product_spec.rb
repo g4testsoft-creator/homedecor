b194915577387ab9e2da261d4499abc6b0bb040c
nasira <nasira@cybernest.com>
2026-03-12 14:25:43 +0500

    fix

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,11 +4,10 @@ RSpec.describe Product, type: :model do
   describe 'associations' do
     it { should belong_to(:category).optional }
     it { should have_many(:reviews).dependent(:destroy) }
-    it { should have_one_attached(:image) }
+    it { should have_many_attached(:image) }
   end
 
   describe 'validations' do
     it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:image) }
   end
 end
