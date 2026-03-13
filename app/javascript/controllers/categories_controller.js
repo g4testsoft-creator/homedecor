3faaef1d3264c433dca9288e32a9a5fcd3147115
nasira <nasira@cybernest.com>
2026-03-13 12:35:31 +0500

    added controller

---
+++ app/javascript/controllers/categories_controller.js
@@ -0,0 +1,35 @@
+// app/javascript/controllers/categories_controller.js
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["menu"]
+  
+  connect() {
+    this.isOpen = false
+    
+    // Close menu when clicking outside
+    document.addEventListener("click", (event) => {
+      if (!this.element.contains(event.target)) {
+        this.close()
+      }
+    })
+  }
+  
+  toggle() {
+    if (this.isOpen) {
+      this.close()
+    } else {
+      this.open()
+    }
+  }
+  
+  open() {
+    this.menuTarget.classList.add("show")
+    this.isOpen = true
+  }
+  
+  close() {
+    this.menuTarget.classList.remove("show")
+    this.isOpen = false
+  }
+}
