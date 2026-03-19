0fcd0e22d8e9bb65001deea459952107cdbdc80a
nasira <nasira@cybernest.com>
2026-03-19 11:07:42 +0500

    fix

--- app/sidekiq/sitemap_refresh_job.rb
+++ app/sidekiq/sitemap_refresh_job.rb
@@ -13,7 +13,6 @@ class SitemapRefreshJob
       
       if result
         Rails.logger.info "Sitemap refresh completed successfully at #{Time.current}"
-        ping_search_engines if Rails.env.production?
       else
         Rails.logger.error "Sitemap refresh command failed"
         raise "Sitemap refresh failed"
@@ -23,19 +22,5 @@ class SitemapRefreshJob
       Rails.logger.error "Sitemap refresh failed: #{e.message}"
       raise e
     end
-  end
-  
-  private
-  
-  def ping_search_engines
-    sitemap_url = "#{Rails.application.routes.default_url_options[:host]}/sitemap.xml"
-    
-    # Ping Google
-    Net::HTTP.get(URI.parse("https://www.google.com/ping?sitemap=#{CGI.escape(sitemap_url)}"))
-    
-    # Ping Bing
-    Net::HTTP.get(URI.parse("https://www.bing.com/ping?sitemap=#{CGI.escape(sitemap_url)}"))
-  rescue => e
-    Rails.logger.warn "Failed to ping search engines: #{e.message}"
   end
 end
